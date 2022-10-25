3ce1091b965747524dbb6290bdfe22b7f1f70262
mrczntt <107638867+mrczntt@users.noreply.github.com>
2022-10-25 13:51:19 +0200

    Added support for CBR and CB7 archives

--- README.md
+++ README.md
@@ -1,5 +1,24 @@
 # cbzconv
 
-A small Python script to convert Comic Book Archives to PDF for easy portability.
+A small Python script to convert comic book archives to PDF for easy portability.
 
-Currently supports .cbz and .cbt archives.
+Supports the following archive formats:
+
+* .CBZ
+* .CBR (you need to have "unrar" in your PATH)
+* .CB7 (you need to have "7z" in your PATH)
+* .CBT
+
+## Dependencies and installation
+
+* python >= 3.10
+* python-pillow >= 9.2.0
+
+Download the ```cbzconv.py``` file, remove the .py extension and move it somewhere in your PATH (ideally /usr/local/bin)
+
+## Basic usage
+
+```bash
+cbzconv "A Comic Book.cbz" "Another Comic Book.cbz"
+```
+
--- cbzconv.py
+++ cbzconv.py
@@ -5,6 +5,7 @@ import tempfile
 import shutil
 import argparse
 import re
+import subprocess
 from PIL import Image
 
 def natural_sort(l): 
@@ -14,9 +15,9 @@ def natural_sort(l):
 
 parser = argparse.ArgumentParser(description='Converts Comic Book Archives to PDF')
 parser.add_argument('input_files', type=str, nargs='+', help='files to be converted')
-parser.add_argument('--output-dir', metavar='path', dest="output_dir", default=".", type=str, help='output directory')
+parser.add_argument('--output-dir', metavar='path', dest='output_dir', default='.', type=str, help='output directory')
 parser.add_argument('--no-landscape', dest='no_landscape', action='store_true', help='flip all landscape-oriented pages')
-parser.add_argument('--paper-size', metavar='size', dest="paper_size", default="SameAsPage", type=str, help='set a fixed paper size')
+parser.add_argument('--paper-size', metavar='size', dest='paper_size', default='SameAsPage', type=str, help='set a fixed paper size')
 
 args = parser.parse_args()
 page_size = (0, 0)
@@ -31,12 +32,12 @@ match args.paper_size:
 	case 'SameAsPage':
 		pass
 	case other:
-		print("Unsupported paper size. Supported values are: A4, A5, B6, SameAsPage (default)")
+		print('Unsupported paper size. Supported values are: A4, A5, B6, SameAsPage (default)')
 		exit()
 
 
 if not os.path.isdir(args.output_dir):
-	print("Output directory doesn't exist")
+	print('Output directory doesn\'t exist')
 	exit()
 		
 for input_file in args.input_files:
@@ -52,6 +53,20 @@ for input_file in args.input_files:
 			shutil.unpack_archive(input_file, temp_dir, 'zip')
 		case '.CBT':
 			shutil.unpack_archive(input_file, temp_dir, 'tar')
+		case '.CBR':
+			unarchiver_path = shutil.which('unrar')
+			if unarchiver_path is not None:
+				subprocess.run([unarchiver_path, 'e', input_file, temp_dir], stdout=subprocess.DEVNULL)
+			else:
+				print('Cannot find unrar in PATH. Skipping.')
+				continue
+		case '.CB7':
+			unarchiver_path = shutil.which('7z')
+			if unarchiver_path is not None:
+				subprocess.run([unarchiver_path, 'e', input_file, '-o' + temp_dir], stdout=subprocess.DEVNULL)
+			else:
+				print('Cannot find 7z in PATH. Skipping.')
+				continue
 		case other:
 			print(f'File {input_file} not recognized. Skipping.')
 			continue
@@ -79,5 +94,4 @@ for input_file in args.input_files:
     	
 	first_page = pages.pop(0)
 	output_file = os.path.join(args.output_dir, basename + '.pdf')
-	first_page.save(output_file, "PDF" , resolution=100.0, save_all=True, append_images=pages)	
-
+	first_page.save(output_file, 'PDF' , resolution=100.0, save_all=True, append_images=pages)	
